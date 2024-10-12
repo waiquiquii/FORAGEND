eddc0f9e72db186e1aba1c61405a774defc94212
Waiquiqui <walnerguaito@hotmail.com>
2024-10-12 12:08:56 -0500

    punto inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Menuvertical from './components/Menuvertical'; // Importa el componente MenuVertical
+import Perfilusuario  from './components/Perfilusuario'; // Importa el componente Perfilusuario
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Menuveritcal" element={<Menuvertical />} />
+        <Route path="/" element={<Perfilusuario />} />
+      </Routes>
+    </Router>
   );
 }
 
